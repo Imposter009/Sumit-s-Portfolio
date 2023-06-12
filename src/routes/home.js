c229831b8ae1bff5414524c2b4724a002b83b221
Sumit <sumitgupta19900@gmail.com>
2023-06-12 16:41:32 +0530

    first commit

---
+++ src/routes/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Main from '../components/main'
+
+const home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Main/>
+    </div>
+  )
+}
+
+export default home
