168b7f3c4af46ca3bf7a6db12cfd583bd17d79f6
Sumit <sumitgupta19900@gmail.com>
2023-06-14 01:20:42 +0530

    14

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/footer'
+import Main2 from '../components/main2'
+
 
 const project = () => {
   return (
     <div>
-      <Navbar/>
-      <Footer/>
+      <Navbar />
+      <Main2  heading="Projects" content="A detail about projects" />
+      <Footer />
     </div>
   )
 }
